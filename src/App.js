e0a0370e354c60af1879ef63402fc03781b401c7
Ramon Ibarra <ramon9805@gmail.com>
2020-09-22 01:47:48 -0500

    Cree la carpeta components, separe la info en componentes, pase el array tasks atraves de props para disponer de él en 'ListItems y PersonalTasks', use el hook useForm, para modidficar el codigo en ListItems y solucionar el desafio 3 de forma diferente, etc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React, { useEffect, useState, useRef } from "react";
-import {
-  Container,
-  Row,
-  Col,
-  ListGroup,
-  Card,
-  Form,
-  Button,
-} from "react-bootstrap";
+
 import "./App.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Persons from "./components/Persons";
+import PersonalTasks from "./components/PersonalTasks";
 
 const tasks = [
   {
@@ -16,21 +11,21 @@ const tasks = [
     title: "Título 1",
     inModification: false,
     checked: false,
-    defaulValue: "",
+    defaulValue: "aaa",
   },
   {
     id: 2,
     title: "Título 2",
     inModification: false,
     checked: true,
-    defaulValue: "",
+    defaulValue: "a",
   },
   {
     id: 3,
     title: "Título 3",
     inModification: false,
     checked: false,
-    defaulValue: "",
+    defaulValue: "aaaaa",
   },
   {
     id: 4,
@@ -42,7 +37,7 @@ const tasks = [
   {
     id: 5,
     title: "Título 5",
-    inModification: true,
+    inModification: false,
     checked: true,
     defaulValue: "",
   },
@@ -56,181 +51,27 @@ const tasks = [
   {
     id: 7,
     title: "Título 7",
-    inModification: true,
+    inModification: false,
     checked: false,
     defaulValue: "",
   },
 ];
 
-function ListItems() {
-  const [mList, setmList] = useState([]);
-  const [change, setChange] = useState([""]);
-
-  const handleChange = (e) => {
-    const doChange = (e) => {
-      setChange({
-        ...change,
-        [e.target.name]: e.target.value,
-      });
-    };
-
-    //Ifs que identifican la procedencia del form, al no contar con "index" como referecia use eso
-    if (e.target.name === "form0") {
-      doChange(e);
-    }
-
-    if (e.target.name === "form1") {
-      doChange(e);
-    }
-
-    if (e.target.name === "form2") {
-      doChange(e);
-    }
-
-    if (e.target.name === "form3") {
-      doChange(e);
-    }
-
-    if (e.target.name === "form4") {
-      doChange(e);
-    }
-
-    if (e.target.name === "form5") {
-      doChange(e);
-    }
-
-    if (e.target.name === "form6") {
-      doChange(e);
-    }
-  };
-
-  return tasks.map((task, index) => {
-    //If necesario para que no se multiplique infinitamente el array cada que se itera con el 'setChange'
-    if (mList.length < 8) {
-      mList.push(task.inModification);
-    }
-
-    const label = <span className="font-weight-bold">{task.title}</span>;
-    const title = (editing) => {
-      return editing ? (
-        <input
-          className="form-control"
-          name={`form${index}`}
-          onChange={handleChange}
-          defaultValue={task.defaulValue}
-          //ref={ref}
-          id={`form${index}`}
-        />
-      ) : (
-        <Form.Check
-          id={`task${task.id}`}
-          type="checkbox"
-          defaultChecked={task.checked}
-          label={label}
-        />
-      );
-    };
-
-    const butt = (show) => {
-      return show ? (
-        <Button
-          onClick={() => {
-            //Seccion de ifs necesaria para que no marque "undefined" al momento de NO asignar un valor al input y pulsar Cancel
-
-            //Tambien en esta zona es donde se modifica el valor de tasks para que en el sigiente render (despues de dar click en "Cancel") se muestre el defaultValue de lo que escribio el usuario
-            if (index === 0 && change.form0 === undefined) {
-              console.log("change.form0 esta indefinido");
-            } else {
-              tasks[0].defaulValue = change.form0;
-            }
-
-            if (index === 1 && change.form1 === undefined) {
-              console.log("change.form1 esta indefinido");
-            } else {
-              tasks[1].defaulValue = change.form1;
-            }
-
-            if (index === 2 && change.form2 === undefined) {
-              console.log("change.form2 esta indefinido");
-            } else {
-              tasks[2].defaulValue = change.form2;
-            }
-
-            if (index === 3 && change.form3 === undefined) {
-              console.log("change.form3 esta indefinido");
-            } else {
-              tasks[3].defaulValue = change.form3;
-            }
-
-            if (index === 4 && change.form4 === undefined) {
-              console.log("change.form4 esta indefinido");
-            } else {
-              tasks[4].defaulValue = change.form4;
-            }
-
-            if (index === 5 && change.form5 === undefined) {
-              console.log("change.form5 esta indefinido");
-            } else {
-              tasks[5].defaulValue = change.form5;
-            }
-
-            if (index === 6 && change.form6 === undefined) {
-              console.log("change.form6 esta indefinido");
-            } else {
-              tasks[6].defaulValue = change.form6;
-            }
-
-            //Al hacer click en edit muestra el input-text y al hacer click en "Cancel" se oculta el input-texto y se muestra el titulo y check
-            tasks[index].inModification = false;
-            setmList(mList.slice(index, 0, false));
-          }}
-        >
-          Cancel
-        </Button>
-      ) : null;
-    };
-
-    return (
-      <ListGroup.Item key={index}>
-        <Row>
-          <Col xs="8" className="my-auto">
-            {title(mList[index])}
-          </Col>
-          <Col xs="2">
-            <Button
-              onClick={() => {
-                //NO sé que vrg hice al 100% pero esta madre jala y al momento del click en 'Edit', el input cambia.
-                tasks[index].inModification = true;
-                setmList(mList.slice(index, 0, true));
-              }}
-            >
-              Edit
-            </Button>
-          </Col>
-          <Col xs="2">{butt(mList[index])}</Col>
-        </Row>
-      </ListGroup.Item>
-    );
-  });
-}
-
-const App = () => (
-  <Container fluid>
-    <Row>
-      <Col xs="8" className="mx-auto mt-3">
-        <Card>
-          <Card.Title className="text-center">
-            <h3 className="mt-3">TODO Tasks</h3>
-          </Card.Title>
-          <Card.Body>
-            <ListGroup>
-              <ListItems />
-            </ListGroup>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
-  </Container>
-);
+const App = () => {
+  let tasksToDo = tasks.length;
+  console.log(tasksToDo);
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Persons tasksToDo={tasksToDo} />
+        </Route>
+        <Route path="/tasks/:id" exa>
+          <PersonalTasks tasks={tasks} />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
